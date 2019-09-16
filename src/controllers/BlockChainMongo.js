87b44d9356b262305925bb2eba1a2822d6c1ba66
IvanBlacky <tcherivan@gmail.com>
2019-09-16 18:48:34 +0300

    #2 Feat: implement getValidators method

--- src/controllers/BlockChainMongo.js
+++ src/controllers/BlockChainMongo.js
@@ -1,12 +1,50 @@
 const core = require('gls-core-service');
 const BasicController = core.controllers.Basic;
+const env = require('../data/env');
+const { MongoClient, ObjectId } = require('mongodb');
 
 class BlockChainMongo extends BasicController {
-    constructor({ connector, mongodb }) {
-        super({ connector });
-        this._connection = mongodb.connection;
+    async boot() {
+        return new Promise((resolve, reject) => {
+            const client = new MongoClient(env.GLS_CYBERWAY_MONGO_CONNECT);
+            client.connect((err, client) => {
+                if (err) {
+                    reject(err);
+                }
+                this._client = client;
+                resolve();
+            });
+        });
     }
-    getDelegations() {}
+    async getDelegations() {}
+
+    async getValidators({ sequenceKey = null, limit = 10 }) {
+        const query = {};
+        if (sequenceKey) {
+            query._id = { $gt: ObjectId(sequenceKey) };
+        }
+
+        const projection = { id: false, _SERVICE_: false };
+
+        const collection = this._client.db('_CYBERWAY_').collection('stake_cand');
+        const validators = await collection
+            .find(query)
+            .project(projection)
+            .sort([['priority', 1]])
+            .limit(limit)
+            .toArray();
+
+        let newSequenceKey = null;
+
+        if (validators.length === limit) {
+            newSequenceKey = validators[validators.length - 1]._id;
+        }
+
+        return {
+            validators,
+            sequenceKey: newSequenceKey,
+        };
+    }
 }
 
 module.exports = BlockChainMongo;
--- src/services/Connector.js
+++ src/services/Connector.js
@@ -3,17 +3,22 @@ const BasicConnector = core.services.Connector;
 const BlockChainMongo = require('../controllers/BlockChainMongo');
 
 class Connector extends BasicConnector {
-    constructor({ mongodb }) {
+    constructor() {
         super();
 
-        this._bcMongo = new BlockChainMongo({ connector: this, mongodb });
+        this._bcMongo = new BlockChainMongo({ connector: this });
     }
 
     async start() {
+        await this._bcMongo.boot();
         await super.start({
             serverRoutes: {
                 getDelegations: {
                     handler: this._bcMongo.getDelegations,
+                    scope: this._bcMongo,
+                },
+                getValidators: {
+                    handler: this._bcMongo.getValidators,
                     scope: this._bcMongo,
                 },
             },
