f1cb7616bfd7e73371fe2538dc1ddecaad25f59e
Denis Zakharenkov <zaharden@gmail.com>
2019-09-27 20:33:51 +0700

    Add encrypted .npmrc and .env to securely build and deploy state-reader

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -3,7 +3,11 @@ set -euo pipefail
 
 IMAGETAG="${BUILDKITE_BRANCH}-${BUILDKITE_BUILD_NUMBER}"
 
+ansible-vault view playbooks/configs/.npmrc.vault --vault-password-file ~/vault_pass > .npmrc
+
 docker build -t cyberway/state-reader:${IMAGETAG} .
+
+rm .npmrc
 
 docker login -u=$DHUBU -p=$DHUBP
 
