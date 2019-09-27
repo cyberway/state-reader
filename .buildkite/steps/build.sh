86cf7cb3309fd6fc4691c3d66332f655c3ac3ca3
Denis Zakharenkov <zaharden@gmail.com>
2019-09-27 22:20:07 +0700

    CI (#17)
    * Add encrypted .npmrc and .env to securely build and deploy state-reader
    * Add ssh private key usage
    * Ignore minor errors with apt cache
    * Dev tag is not the same as production

---
+++ .buildkite/steps/build.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+set -euo pipefail
+
+IMAGETAG="dev-${BUILDKITE_BRANCH}-${BUILDKITE_BUILD_NUMBER}"
+
+ansible-vault view playbooks/configs/.npmrc.vault --vault-password-file ~/vault_pass > .npmrc
+
+docker build -t cyberway/state-reader:${IMAGETAG} .
+docker push cyberway/state-reader:${IMAGETAG}
+
+rm .npmrc
+
+docker login -u=$DHUBU -p=$DHUBP
+
+if [[ "${BUILDKITE_TAG}" != "" ]]; then
+    docker tag cyberway/state-reader:${IMAGETAG} cyberway/state-reader:${BUILDKITE_TAG}
+    docker push cyberway/state-reader:${BUILDKITE_TAG}
+    
+    docker tag cyberway/state-reader:${IMAGETAG} cyberway/state-reader:latest
+    docker push cyberway/state-reader:latest
+fi
