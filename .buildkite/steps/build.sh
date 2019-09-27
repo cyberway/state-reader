375db183d0e859980a5fe46e8d3d86e201a63a80
Denis Zakharenkov <zaharden@gmail.com>
2019-09-28 01:22:13 +0700

    Fix

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -4,9 +4,8 @@ set -euo pipefail
 IMAGETAG="dev-${BUILDKITE_BRANCH}-${BUILDKITE_BUILD_NUMBER}"
 
 ansible-vault view playbooks/configs/.npmrc.vault --vault-password-file ~/vault_pass > .npmrc
-
 docker build -t cyberway/state-reader:${IMAGETAG} .
+rm .npmrc
+
 docker login -u=$DHUBU -p=$DHUBP
 docker push cyberway/state-reader:${IMAGETAG}
-
-rm .npmrc
--- .buildkite/steps/release-image.sh
+++ .buildkite/steps/release-image.sh
@@ -3,10 +3,9 @@ set -euo pipefail
 
 IMAGETAG="dev-${BUILDKITE_BRANCH}-${BUILDKITE_BUILD_NUMBER}"
 
-docker login -u=$DHUBU -p=$DHUBP
-docker pull cyberway/state-reader:${IMAGETAG}
-
 if [[ "${BUILDKITE_TAG}" != "" ]]; then
+    docker login -u=$DHUBU -p=$DHUBP
+    docker pull cyberway/state-reader:${IMAGETAG}
     docker tag cyberway/state-reader:${IMAGETAG} cyberway/state-reader:${BUILDKITE_TAG}
     docker push cyberway/state-reader:${BUILDKITE_TAG}
 fi
