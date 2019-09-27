cb68532339234201e3dc4d0f34e121f2e53e9b60
Denis Zakharenkov <zaharden@gmail.com>
2019-09-27 21:57:30 +0700

    Fix: dev tag is not the same as production

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -1,11 +1,12 @@
 #/bin/bash
 set -euo pipefail
 
-IMAGETAG="${BUILDKITE_BRANCH}-${BUILDKITE_BUILD_NUMBER}"
+IMAGETAG="dev-${BUILDKITE_BRANCH}-${BUILDKITE_BUILD_NUMBER}"
 
 ansible-vault view playbooks/configs/.npmrc.vault --vault-password-file ~/vault_pass > .npmrc
 
 docker build -t cyberway/state-reader:${IMAGETAG} .
+docker push cyberway/state-reader:${IMAGETAG}
 
 rm .npmrc
 
@@ -13,7 +14,8 @@ docker login -u=$DHUBU -p=$DHUBP
 
 if [[ "${BUILDKITE_TAG}" != "" ]]; then
     docker tag cyberway/state-reader:${IMAGETAG} cyberway/state-reader:${BUILDKITE_TAG}
-    docker tag cyberway/state-reader:${IMAGETAG} cyberway/state-reader:latest
     docker push cyberway/state-reader:${BUILDKITE_TAG}
+    
+    docker tag cyberway/state-reader:${IMAGETAG} cyberway/state-reader:latest
     docker push cyberway/state-reader:latest
 fi
